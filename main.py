1828a41efcd576cb628b861f0141990d640d7d96
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-09-30 21:32:44 +0800

    feat: 新增程序入口

---
+++ main.py
@@ -0,0 +1,7 @@
+from maa.toolkit import Toolkit
+
+def main():
+    Toolkit.pi_run_cli("C:/Users/DELL/Desktop/github/MST/", "C:/Users/DELL/Desktop/github/MST/assets/")
+
+if __name__ == "__main__":
+    main()
