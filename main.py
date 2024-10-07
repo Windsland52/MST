50fa89cc3cf1ef4a5ead66a8bb27b7edaa13e1a9
Windsland52 <86581225+Windsland52@users.noreply.github.com>
2024-10-07 17:18:39 +0800

    chore: 更新列表

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from maa.toolkit import Toolkit
+from maa.context import Context
+from maa.custom_action import CustomAction
+from maa.custom_recognition import CustomRecognition
 
 def main():
     Toolkit.pi_run_cli("C:/Users/DELL/Desktop/github/MST/", "C:/Users/DELL/Desktop/github/MST/assets/")
